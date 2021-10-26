401469975744bfaf589a1253be2e16da1f526ee3
ranor_seis <46585507+ranoriginals@users.noreply.github.com>
2021-10-25 21:17:17 -0400

    Create quakeml2dat.py

---
+++ 1_Earthquake_catalog/quakeml2dat.py
@@ -0,0 +1,18 @@
+from obspy import read_events
+
+cat = read_events("*.quakeml")
+
+with open("AACSE_catalog.dat", "w") as fcat:
+    for event in cat:
+        origin = event.origins[0]
+        magnitude = event.magnitudes[0]
+        fcat.write(
+            "{} {:9.4f} {:8.4f} {:5.1f} {:3.1f} {}\n".format(
+                origin.time,
+                origin.longitude,
+                origin.latitude,
+                origin.depth / 1000.0,
+                magnitude.mag,
+                event.resource_id.id.split("/")[2],
+            )
+        )
