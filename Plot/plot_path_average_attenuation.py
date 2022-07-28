005fc06b8580a482ce3a0fbb669f664f40307162
zhuoran <ranoriginals@gmail.com>
2022-07-28 12:12:19 -0400

    modify plot script

--- Plot/plot_path_average_attenuation.py
+++ Plot/plot_path_average_attenuation.py
@@ -28,4 +28,27 @@ for event in pstar_data:
         output = os.popen(cmd).read().strip()
         stlo = float(output.split()[2])
         stla = float(output.split()[3])
-        
+        ax.scatter(evlo, evla, evdp, marker="o", s=2, alpha=0.5, color="black", label="Events")
+        ax.scatter(stlo, stla, 0, marker="^", s=2, alpha=0.5, color="black", label="Stations")
+        x = [evlo, stlo]
+        y = [evla, stla]
+        z = [evdp, 0]
+        ax.plot(x, y, z, c=new_jet.to_rgba(atten), linewidth=0.5)
+
+ax.set_xlabel("Longitude")
+ax.set_ylabel("Latitude")
+ax.set_zlabel("Depth (Km)")
+ax.invert_zaxis()
+l = 0.9
+b = 0.25
+w = 0.02
+h = 0.6
+rect = [l,b,w,h]
+cbar_ax = fig.add_axes(rect)
+fig.colorbar(new_jet, orientation="vertical", label="1000/Qp", cbar=cbar_ax)
+ax.view_init(elev=10, azim=120)
+fig.suptitle("Trench Parallel")
+plt.savefig("Trench_parallel.pdf")
+ax.view_init(elev=10, azim=-150)
+fig.suptitle("Trench Normal")
+plt.savefig("Trench_normal.pdf")
